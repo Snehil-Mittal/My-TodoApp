16fa59a08af26b3be6ad68fec7d7298999ff3d95
Snehil-Mittal <snehilmittal2001@gmail.com>
2021-07-20 02:26:48 +0530

    Add todo styling

--- src/Components/Addtodo.js
+++ src/Components/Addtodo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class Addtodo extends React.Component{
     state={
-        content:null
+        content:''
     }
 
     handleChange =(e)=>{
@@ -14,15 +14,25 @@ class Addtodo extends React.Component{
     handleSubmit=(e)=>{
         e.preventDefault();
         this.props.addtodo(this.state);
+        this.setState({
+            content:''
+        })
     }
     render(){
         return(
-            <div className="addtodo">
+            <div className="addtodo row">
                 <form onSubmit={this.handleSubmit} >
+                <div className="col s6">
                     <label className="black-text">Add new Todo</label>
-                    <input type='text' id="content" onChange={this.handleChange}></input>
+                    <input type='text' id="content" onChange={this.handleChange} required value={this.state.content}></input>
+                </div>
+                <div className="col s6">
+                    <button onClick={this.HandleSubmit} type="submit">
+                         Add it!
+                    </button>
+                </div>
                 </form>
-            </div>
+                </div>
         )
     }
 }
